0f92f667cb869eda5aa65d050568ab969fdd3721
Natasha Pierre-Louis <natasha.pierre-louis@gsa.gov>
2024-10-28 12:29:52 -0600

    Updated the site.js script

--- _data/site.js
+++ _data/site.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
 
-const baseUrl = process.env.BASE_URL || 'http://localhost:8080';
+// Determine if the environment is production
+const isProduction = process.env.ELEVENTY_ENV === 'production';
+
+// Use BASE_URL for production, otherwise default to localhost for local development
+const baseUrl = isProduction
+  ? process.env.BASE_URL || 'https://federalist-ed45f93b-f174-4670-8752-8fcef170bc2d.sites.pages.cloud.gov/preview/gsa-tts/10x-vop-pra/'
+  : 'http://localhost:8080';
+
 const urlObject = new URL(baseUrl);
 
 module.exports = {
